c276f63d3265aad1a904143f5838240512b8b943
gmnawab <gmnawab25@gmail.com>
2024-06-15 18:32:49 +0600

    created the cart data getting functionality

--- index.js
+++ index.js
@@ -48,6 +48,14 @@ async function run() {
       const result = await cartCollection.insertOne(cartData);
       res.send(result);
     })
+
+    //get cart data
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
+      const query = {email: email}
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+  })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
